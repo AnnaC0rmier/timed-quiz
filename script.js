16c081aed27393745c241e3af0f0cc30c11e1622
AnnaC0rmier <anna.cormier.96@gmail.com>
2023-12-30 17:26:28 -0600

    created questions

---
+++ script.js
@@ -0,0 +1,58 @@
+var quizQuestions = [
+    {
+        question: 'What does HTML stand for?',
+        choices: {
+            a: 'How to make lasanga',
+            b: 'Hyper Text Markup Language',
+            c: 'Hyper Text Markdown Language',
+            d: 'it doesnt stand for anything',
+        },
+        answer: 'b'
+    },
+
+    {
+        question: 'What is the symbol in css used to select an element by id',
+        choices: {
+            a: '!',
+            b: '*',
+            c: '.',
+            d: '#',
+        },
+        answer: 'd'
+    },
+
+    {
+        question: 'which answer choice does NOT represent a way to define a variable',
+        choices: {
+            a: 'assign',
+            b: 'let',
+            c: 'const',
+            d: 'var',
+        },
+        answer: 'a'
+    },
+
+    {
+        question: 'where do you link a CSS file within the HTML file',
+        choices: {
+            a: 'in the head element',
+            b: 'in the main element',
+            c: 'in a div element',
+            d: 'in the body element',
+        },
+        answer: 'a'
+    },
+
+    {
+        question: 'what is the CSS property used to change text color',
+        choices: {
+            a: 'text-color',
+            b: 'color',
+            c: 'change-text-color',
+            d: 'text',
+        },
+        answer: 'b'
+    },
+
+
+    function displayQuestion()
